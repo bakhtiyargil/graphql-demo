3ce84b548977e0912cd129fa8b4d3a23bfc536bf
bakhtiyargil <bext97@mail.ru>
2024-01-12 12:23:37 +0400

    new entities added

---
+++ src/main/java/az/baxtiyargil/graphqldemo/model/entity/CustomerPropertyValue.java
@@ -0,0 +1,49 @@
+package az.baxtiyargil.graphqldemo.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+
+@Entity
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "customer_property_value")
+public class CustomerPropertyValue implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "customer_id")
+    @ToString.Exclude
+    private Customer customer;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "property_id")
+    @ToString.Exclude
+    private CustomerProperty customerProperty;
+
+    private String value;
+
+}
